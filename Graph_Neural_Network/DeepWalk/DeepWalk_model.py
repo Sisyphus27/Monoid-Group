d79908ca287a78c6abd54d99f6d7ed5b677900b7
zy <rorschach_zhao@outlook.com>
2022-08-23 16:34:40 +0800

    GNN

---
+++ Graph_Neural_Network/DeepWalk/DeepWalk_model.py
@@ -0,0 +1,9 @@
+#!/user/bin/env python
+# coding=utf-8
+"""
+@project : Monoid-Group
+@author  : zy
+@file   : DeepWalk_model.py
+@ide    : PyCharm
+@time   : 2022/8/23 16:34
+"""
---
+++ Graph_Neural_Network/README.md
@@ -0,0 +1,2 @@
+# 4th > What does this directory do?
+    Fulfill Graph Neural Network relatated algorithms.
